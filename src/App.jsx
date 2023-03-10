43337bcb7b0ac456d61dd820c15e5e8709c4e706
Anees Muzzafer <aneesmuzzafer@gmail.com>
2023-03-10 20:07:35 +0530

    🐛 Fixed import router issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@
 
 import React from 'react'
 import { RouterProvider } from 'react-router-dom';
-import { authRouter } from './Navigation/router.jsx';
+import { authRouter } from './navigation/router.jsx';
 
 function App() {
     return <RouterProvider router={authRouter} />;
